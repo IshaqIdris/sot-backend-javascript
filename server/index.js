27813804771b30534014692a423a9736158fc8b3
Ishaq Idris <idrismoha2@ecs.vuw.ac.nz>
2018-05-28 18:41:02 +1200

    Added recognition to index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, '../public')));
+//app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('Our app is listening on port 3000!'));
